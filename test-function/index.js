bdd2fa10c55ddff23cdbdd6c58653d518bb8ac97
Krishnan Mani <km@krishnanm.com>
2018-01-25 18:11:25 +0530

    scheduled test function now works with put-item

--- test-function/index.js
+++ test-function/index.js
@@ -1,6 +1,29 @@
 'use strict';
 
+let AWS = require("aws-sdk");
+
 exports.handler = (event, context, callback) => {
     console.log("invoked at " + Date.now());
-    callback(null, "success");
+
+    let tableName = process.env.TABLE_NAME;
+    let dynamodb = new AWS.DynamoDB();
+    let params = {
+        Item: {
+            "id": {
+                S: Date.now().toString()
+            }
+        },
+        TableName: tableName
+    };
+
+    dynamodb.putItem(params, function (err, data) {
+        if (err) {
+            console.error(err, err.stack);
+            callback(err);
+        }
+        else {
+            console.log(data);
+            callback(null, data);
+        }
+    });
 }
